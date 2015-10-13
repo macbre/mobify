166dcb05d5fdab3d6d10c979a10d8fa8cd2af023
Maciej Brencz <maciej.brencz@gmail.com>
2015-10-13 20:21:01 +0200

    Import docopt

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
+from mobify.version import version
+
 # @see https://github.com/pypa/sampleproject/blob/master/setup.py
 setup(
     name='mobify',
-    version='0.1.0',
+    version=version,
     author='Maciej Brencz',
     author_email='maciej.brencz@gmail.com',
     description='Download a webpage as an e-book',
     keywords='epub mobi ebook html converter',
     packages=find_packages(),
     install_requires=[
+        'docopt==0.6.2',
         'ebooklib==0.15',
         'lxml==3.4.0',
         'pylint==1.4.4',
