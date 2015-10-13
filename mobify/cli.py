87face94ae87ee108b1d15da5d1be90a15d16f71
Maciej Brencz <maciej.brencz@gmail.com>
2015-10-13 20:23:35 +0200

    mobify: use docopt to parse the cli argument

--- mobify/cli.py
+++ mobify/cli.py
@@ -1,31 +1,53 @@
+"""mobify
+
+Download a web page (set of web pages) as an e-book.
+
+Usage:
+  mobify URL ...
+  mobify (-h | --help)
+  mobify --version
+
+Options:
+  URL           Space-separated list of URLs to fetch.
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+
 import logging
 import subprocess
 
+from docopt import docopt
+
+from .errors import MobifyError
 from .publisher import Publisher
+from .version import version
+
 
 def main():
     """ Main entry point for CLI"""
-    # TODO: handle getopt
-
     logger = logging.getLogger(__name__)
 
-    chapters = [
-        'http://histmag.org/Margaret-Thatcher-droga-do-premierostwa-7895',
-        'http://histmag.org/Margaret-Thatcher-tajfun-reform-7896',
-    ]
+    arguments = docopt(__doc__, version='mobify {}'.format(version))
+    logger.debug('Options: {}'.format(arguments))
 
-    #chapters = ['http://histmag.org/Wielkie-Ksiestwo-Poznanskie-nieudany-eksperyment-polskiej-autonomii-10643;0']
-
+    chapters = arguments['URL']
     logger.info('URL:   {}'.format(chapters))
 
-    publisher = Publisher(chapters=chapters)
-    publisher.publish()
+    try:
+        publisher = Publisher(chapters=chapters)
+        publisher.publish()
 
-    # convert epub to mobi
-    epub_file = publisher.get_dest()
-    mobi_file = publisher.get_dest('mobi')
-    logger.info('Converting to mobi: {}'.format(mobi_file))
+        # convert epub to mobi
+        epub_file = publisher.get_dest()
+        mobi_file = publisher.get_dest('mobi')
+        logger.info('Converting to mobi: {}'.format(mobi_file))
 
-    subprocess.call(['ebook-convert', epub_file, mobi_file], stderr=subprocess.STDOUT)
+        subprocess.call(['ebook-convert', epub_file, mobi_file], stderr=subprocess.STDOUT)
 
-    logger.info('Converting completed')
+        logger.info('Converting completed')
+
+    except MobifyError as ex:
+        logger.error('Failed to generate an ebook', exc_info=True)
+
+        print ex
+        exit(2)
