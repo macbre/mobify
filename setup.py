0d5a25d72511e4e198096217151862b87070cc4f
Maciej Brencz <maciej.brencz@gmail.com>
2015-10-14 23:02:36 +0200

    Remove support for Python 2.6 and 3.2.

--- setup.py
+++ setup.py
@@ -27,10 +27,8 @@ setup(
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
     ]
