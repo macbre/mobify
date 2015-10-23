81d3032f2d3f415abf6cfdc518ddf21baa550f95
Maciej Brencz <maciej.brencz@gmail.com>
2015-10-23 22:32:34 +0200

    Introduce WikipediaSource

---
+++ mobify/sources/wikipedia.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import re
+try:
+    from urllib import quote  # Py 2.x
+except ImportError:
+    from urllib.parse import quote  # Py 3.x
+
+from mobify.source import MobifySource
+
+
+class WikipediaSource(MobifySource):
+
+    HEADER = u"""
+<h1>{title}</h1>
+<p><small>Z Wikipedii, wolnej encyklopedii</small><br></p>
+"""
+
+    FOOTER = u"""
+<br><br>
+<hr>
+<p><small>Tekst udostępniany na <a href="https://creativecommons.org/licenses/by-sa/3.0/deed.pl">licencji Creative Commons: uznanie autorstwa, na tych samych warunkach</a>, z możliwością obowiązywania dodatkowych ograniczeń.
+Zobacz szczegółowe informacje o <a href="https://wikimediafoundation.org/wiki/Warunki_korzystania">warunkach korzystania</a></small></p>
+
+<p><small><strong>Źródło</strong>: <a href="{url}">{url}</a></small></p>
+    """
+
+    # e.g. https://pl.wikipedia.org/wiki/Kirkja
+    URL_RE = r'https?://([^/]+)/wiki/(.*)$'
+
+    def set_up(self):
+        self._url = self.extend_url(self._url)
+
+    @staticmethod
+    def extend_url(url):
+        # https://pl.wikipedia.org/wiki/Kirkja
+        # https://pl.wikipedia.org/w/index.php?title=Kirkja&printable=yes
+        matches = re.match(WikipediaSource.URL_RE, url)
+
+        return 'https://{domain}/w/index.php?title={title}&printable=yes'.format(
+            domain=matches.group(1), title=matches.group(2)) if matches else None
+
+    @staticmethod
+    def is_my_url(url):
+        return re.match(WikipediaSource.URL_RE, url) is not None
+
+    def get_html(self):
+        article = self.xpath('//*[@id="mw-content-text"]')
+
+        # clean up the HTML
+        xpaths = [
+            'div[div[@class="thumbinner"]]',  # images
+            'div[@class="toc"]',  # TOC
+            'table',  # infobox
+            '*//div[contains(@class, "noprint")]',  # non printable content
+            '*//span[@class="mw-editsection"]',  # edit sections
+        ]
+        article = self.remove_nodes(article, xpaths)
+
+        html = self.get_node_html(article)
+
+        # remove links
+        html = re.sub(r'</?(a)[^>]*>', '', html)
+
+        # add a title and a footer
+        return '\n'.join([
+            self.HEADER.format(title=self.get_title(), author=self.get_author()).strip(),
+            html,
+            self.FOOTER.format(url=self._url).strip()
+        ])
+
+    def get_title(self):
+        return self.get_node('//h1').strip()
+
+    def get_author(self):
+        return self.get_node('//*[@id="siteSub"]')
+
+    def get_language(self):
+        return self.get_node('//h1', 'lang')
---
+++ mobify/test/test_wikipedia.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from . import MobifyTestCase
+from mobify.sources import WikipediaSource
+
+
+class WikipediaSourceTest(MobifyTestCase):
+
+    _source = None
+
+    def setUp(self):
+        self._source = WikipediaSource(
+            url='',
+            content=self.get_fixture('wikipedia.html')
+        )
+
+    @staticmethod
+    def test_is_my_url():
+        assert not WikipediaSource.is_my_url('http://example.com')
+
+        assert not WikipediaSource.is_my_url(
+            'https://pl.wikipedia.org/w/index.php?title=Religia_S%C5%82owian&printable=yes')
+
+        assert WikipediaSource.is_my_url('http://pl.wikipedia.pl/wiki/Foo')
+        assert WikipediaSource.is_my_url('http://pl.wikipedia.pl/wiki/Foo/Bar')
+
+        assert WikipediaSource.is_my_url('http://no.wikipedia.pl/wiki/Foo')
+
+    @staticmethod
+    def test_extend_url():
+        assert WikipediaSource.extend_url(
+            'https://pl.wikipedia.org/wiki/Religia_S%C5%82owian'
+        ) == 'https://pl.wikipedia.org/w/index.php?title=Religia_S%C5%82owian&printable=yes'
+
+        assert WikipediaSource.extend_url(
+            'http://pl.wikipedia.pl/wiki/Foo %26 Bar'
+        ) == 'https://pl.wikipedia.pl/w/index.php?title=Foo %26 Bar&printable=yes'
+
+    def test_parsing(self):
+        assert self._source.get_title() == 'Streymoy'
+        assert self._source.get_author() == 'Z Wikipedii, wolnej encyklopedii'
+        assert self._source.get_language() == 'pl'
+
+        html = self._source.get_html()
+        print(html)  # failed assert will print the raw HTML
+
+        assert '<h1>Streymoy</h1>' in html
+        assert u'<b>Streymoy</b> (duń. <i>Strømø</i>)' in html, "Basic HTML formatting should be kept"
+
+        assert u'<h2>Spis treści</h2>' not in html, "TOC should be removed"
+        assert 'edytuj kod' not in html, "Edit section should be removed"
+        assert 'Kategoria' not in html, "Categories should be removed"
+        assert 'Akwen' not in html, "Infobox should be removed"
+        assert 'Osobny artykuł:' not in html, "Non printable content should be removed"
