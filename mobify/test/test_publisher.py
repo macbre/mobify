700604903fa5f0bc0d92f114a87810483dc7e8c2
Maciej Brencz <maciej.brencz@gmail.com>
2015-11-03 21:13:35 +0100

    Improve the way file names are generated from URLs

--- mobify/test/test_publisher.py
+++ mobify/test/test_publisher.py
@@ -11,6 +11,7 @@ class PublisherTest(TestCase):
         assert Publisher.get_dest_from_chapters([]) is None
         assert Publisher.get_dest_from_chapters(['foo']) == 'foo'
         assert Publisher.get_dest_from_chapters(['http://example.com/foo']) == 'foo'
+        assert Publisher.get_dest_from_chapters(['http://example.com/foo/']) == 'foo'
         assert Publisher.get_dest_from_chapters(['http://example.com/foo/bar']) == 'bar'
         assert Publisher.get_dest_from_chapters(
             ['https://pl.wikipedia.org/wiki/Religia_S%C5%82owian']) == 'Religia_S_C5_82owian'
