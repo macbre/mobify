111e198de12a6d05afd4978aa30df3c4f8c3e8aa
Maciej Brencz <maciej.brencz@gmail.com>
2016-01-21 23:50:33 +0100

    Blogspot: add a source

--- mobify/sources/__init__.py
+++ mobify/sources/__init__.py
@@ -1,4 +1,5 @@
 from .artpl import NosferatuMainSource
+from .blogspot import BlogSpotSource
 from .harp import HarpMainSource
 from .histmag import HistmagSource
 from .wikipedia import WikipediaSource
