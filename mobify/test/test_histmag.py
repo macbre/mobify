43b48e7686f2e620c3f1fb201c17dfe9eca0ac53
Maciej Brencz <maciej.brencz@gmail.com>
2016-12-23 22:35:05 +0100

    Histmag: handle articles with no lead and e-book snippets

--- mobify/test/test_histmag.py
+++ mobify/test/test_histmag.py
@@ -7,15 +7,6 @@ from mobify.sources.histmag import HistmagPage, HistmagSource
 
 class Histmag(MobifyTestCase):
 
-    _source = None
-
-    def setUp(self):
-        # @see https://histmag.org/Maurycy-Beniowski-bunt-na-Kamczatce-13947
-        self._source = HistmagPage(
-            url='',
-            content=self.get_fixture('Maurycy-Beniowski-bunt-na-Kamczatce.html')
-        )
-
     @staticmethod
     def test_is_my_url():
         assert not HistmagSource.is_my_url('http://example.com')
@@ -42,6 +33,18 @@ class Histmag(MobifyTestCase):
             'https://histmag.org/Prawdziwy-powod-wybuchu-I-wojny-swiatowej-9648?ciekawostka'
         ) == 'https://histmag.org/Prawdziwy-powod-wybuchu-I-wojny-swiatowej-9648'
 
+
+class HistmagBeniowski(MobifyTestCase):
+
+    _source = None
+
+    def setUp(self):
+        # @see https://histmag.org/Maurycy-Beniowski-bunt-na-Kamczatce-13947
+        self._source = HistmagPage(
+            url='',
+            content=self.get_fixture('Maurycy-Beniowski-bunt-na-Kamczatce.html')
+        )
+
     def test_parsing(self):
         assert self._source.get_title() == 'Maurycy Beniowski - bunt na Kamczatce'
         assert self._source.get_lead() == u'Po upadku konfederacji barskiej został zesłany na Kamczatkę. Awanturnicza natura nie pozwoliła mu jednak długo zagrzać tam miejsca. Tak Maurycy Beniowski stanął na czele buntu. Czy wywalczył upragnioną wolność?'
@@ -58,3 +61,30 @@ class Histmag(MobifyTestCase):
         assert 'Kamczatka, ilustracja' not in html
         assert 'Maurycy Beniowski (1741-1786) (domena publiczna)' not in html
         assert u'<h4>Zobacz także:</h4>' not in html
+
+
+class HistmagChurchill(MobifyTestCase):
+
+    _source = None
+
+    def setUp(self):
+        # @see https://histmag.org/Winston-Churchill-lew-Albionu-14521
+        self._source = HistmagPage(
+            url='',
+            content=self.get_fixture('Winston-Churchill-lew-Albionu-14521.html')
+        )
+
+    def test_parsing(self):
+        assert self._source.get_title() == 'Winston Churchill – lew Albionu'
+        assert self._source.get_lead() == ''
+        assert self._source.get_author() == u'Michał Gadziński'
+        assert self._source.get_language() == 'pl'
+
+        html = self._source.get_html()
+        print(html)  # failed assert will print the raw HTML
+
+        assert '<h1>Winston Churchill – lew Albionu</h1>' in html
+        assert '<h3>Potomek księcia Marlborough</h3>' in html
+        assert '<p>Winston Leonard Spencer-Churchill przyszedł na świat 30 listopada 1874 roku. ' in html
+
+        assert u'<h3>Tekst jest fragmentem e-booka Michała Gadzińskiego „Perły imperium brytyjskiego”:</h3>' not in html
