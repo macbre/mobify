a7ee91a227ec79c15be87a16902a239eab4a6ca9
Maciej Brencz <maciej.brencz@gmail.com>
2017-03-18 11:59:38 +0100

    Add medium.com source (resolves #30)

--- mobify/sources/__init__.py
+++ mobify/sources/__init__.py
@@ -4,6 +4,7 @@ from .blogspot import BlogSpotSource
 from .flask import FlaskSource
 from .harp import HarpMainSource
 from .histmag import HistmagSource
+from .medium import MediumSource
 from .rust import RustBookSource
 from .tumblr import TumblrArchiveSource
 from .wikipedia import WikipediaSource
